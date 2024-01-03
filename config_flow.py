87ae0ed4880ebe800fad435d070da2a0ffe24adf
Martijn <martijn.diks@gmail.com>
2024-01-03 16:27:36 +0100

    added option for a device name, changed validations, removed some "button plus" things (my example source ;) )

--- config_flow.py
+++ config_flow.py
@@ -3,17 +3,17 @@ from __future__ import annotations
 
 import ipaddress
 import logging
+import re
 import traceback
-import os
-
-from homeassistant import config_entries, exceptions
-from homeassistant.const import CONF_IP_ADDRESS
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-
-from .const import DOMAIN
 
 import snap7
+import getmac
+from homeassistant import config_entries
+from homeassistant.const import CONF_IP_ADDRESS
+import voluptuous as vol
+
+from .const import *
+from .common import get_lib_location
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -32,46 +32,59 @@ class SiemensS7ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_logo(self, user_input=None):
         """Siemens PLC configuration step for Logo! PLC's"""
         errors = {}
+        name = None
         ip = None
         local_tsap = None
         remote_tsap = None
+        logo_connection_result = None
         
         _LOGGER.debug(f"{DOMAIN} Siemens Logo! PLC step {user_input}")
 
         if user_input is not None:
+            name              = user_input["name"]
             ip                = user_input.get(CONF_IP_ADDRESS, None)
             local_tsap        = user_input["local_tsap"]
             remote_tsap       = user_input["remote_tsap"]
 
-            valid_ip          = self.validate_ip(ip) 
+            valid_ip          = self.validate_ip(ip)
             valid_local_tsap  = self.validate_tsap_id(local_tsap) 
             valid_remote_tsap = self.validate_tsap_id(remote_tsap)
 
             if valid_ip and valid_local_tsap and valid_remote_tsap:
                 try:
                     _LOGGER.debug(f"{DOMAIN} Connecting PLC at ip: {ip}")
-                    client = snap7.logo.Logo(lib_location=self.get_lib_location())
-                    result = client.connect(ip, int(local_tsap, 16), int(remote_tsap, 16))
-                    if result == None:
+                    library_location = get_lib_location()
+                    snap7.logo.load_library(library_location)
+                    client = snap7.logo.Logo()
+                    logo_connection_result = client.connect(ip, int(local_tsap, 16), int(remote_tsap, 16))
+                    if logo_connection_result is None:
                         client.disconnect()
                         return self.async_create_entry(
-                            title=f"{device_config.core.name}",
-                            description=f"Base module on {ip} with id {device_config.info.device_id}",
-                            data={"config": json_config}
+                            title=f"{name}",
+                            description=f"Siemens Logo! on {ip} with id s7_{ getmac.get_mac_address(ip=ip).replace(':', '') }",
+                            data={
+                                "type": "logo",
+                                "id": f"logo_{ getmac.get_mac_address(ip=ip).replace(':', '') }",
+                                "connection": {
+                                    "ip": ip,
+                                    "local_tsap": local_tsap,
+                                    "remote_tsap": remote_tsap
+                                }
+                            }
                         )
                     else:
                         raise ConnectionError
 
 
-                except ConnectionError as ex:  # pylint: disable=broad-except
+                except ConnectionError as ex:
                     _LOGGER.error(
-                        f"{DOMAIN} Could not connect the Siemens Logo! with IP {ip}, local_tsap {local_tsap}, remote_tsap {remote_tsap}, result {result}\n Exception: %s - traceback: %s",
+                        f"{DOMAIN} Could not connect the Siemens Logo! with IP {ip}, local_tsap {local_tsap}, remote_tsap {remote_tsap}, connection_result {logo_connection_result}\n Exception: %s - traceback: %s",
                         ex,
                         traceback.format_exc()
                     )
                     errors["base"] = "error_cannot_connect"
 
-                except Exception as ex:  # pylint: disable=broad-except
+                except Exception as ex:
                     _LOGGER.error(
                         f"{DOMAIN} Exception: %s - traceback: %s",
                         ex,
@@ -91,6 +104,7 @@ class SiemensS7ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="logo",
             data_schema=vol.Schema(
                 {
+                    vol.Required("name"): vol.All(str, vol.Length(2, 128)),
                     CONF_IP_ADDRESS: str,
                     vol.Required("local_tsap"): vol.All(str, vol.Length(4, 4), vol.Match(r'^[a-fA-F0-9]+$')),
                     vol.Required("remote_tsap"): vol.All(str, vol.Length(4, 4), vol.Match(r'^[a-fA-F0-9]+$')),
@@ -98,6 +112,7 @@ class SiemensS7ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             ),
             errors=errors ,
             description_placeholders={
+                "name": name,
                 "ip": ip,
                 "local_tsap": local_tsap,
                 "remote_tsap": remote_tsap
@@ -107,46 +122,58 @@ class SiemensS7ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_s7(self, user_input=None):
         """Siemens PLC configuration step for Logo! PLC's"""
         errors = {}
+        name = None
         ip = None
         rack = None
         slot = None
+        s7_connection_result = None
 
         _LOGGER.debug(f"{DOMAIN} Siemens S7 PLC step {user_input}")
 
         if user_input is not None:
+            name = user_input["name"]
             ip   = user_input.get(CONF_IP_ADDRESS, None)
             rack = user_input["rack"]
             slot = user_input["slot"]
 
-            valid_ip   = self.validate_ip(ip) 
-            valid_rack = self.validate_rack(rack) 
-            valid_slot = self.validate_slot(slot)
+            valid_ip   = self.validate_ip(ip)
+            valid_rack = self.validate_rack_slot(rack)
+            valid_slot = self.validate_rack_slot(slot)
 
             if valid_ip and valid_rack and valid_slot:
                 try:
                     _LOGGER.debug(f"{DOMAIN} Connecting PLC at ip: {ip}")
-                    client = snap7.client.Client(lib_location=self.get_lib_location())
-                    result = client.connect(ip, rack, slot) 
-                    if result == 0:
+                    library_location = get_lib_location()
+                    client = snap7.client.Client(lib_location=library_location)
+                    s7_connection_result = client.connect(ip, rack, slot)
+                    if s7_connection_result == 0:
                         client.disconnect()
                         return self.async_create_entry(
-                            title=f"{device_config.core.name}",
-                            description=f"Base module on {ip} with id {device_config.info.device_id}",
-                            data={"config": json_config}
+                            title=f"{name}",
+                            description=f"Siemens S7 PLC on {ip} with id s7_{ getmac.get_mac_address(ip=ip).replace(':', '') }",
+                            data={
+                                "type": "s7",
+                                "id": f"s7_{ getmac.get_mac_address(ip=ip).replace(':', '') }",
+                                "connection": {
+                                    "ip": ip,
+                                    "rack": rack,
+                                    "slot": slot
+                                }
+                            }
                         )
                     else:
                         raise ConnectionError
 
 
-                except ConnectionError as ex:  # pylint: disable=broad-except
+                except ConnectionError as ex:
                     _LOGGER.error(
-                        f"{DOMAIN} Could not connect the Siemens S7 PLC with IP {ip}, rack {rack}, slot {slot}, result {result}\n Exception: %s - traceback: %s",
+                        f"{DOMAIN} Could not connect the Siemens S7 PLC with IP {ip}, rack {rack}, slot {slot}, s7_connection_result {s7_connection_result}\n Exception: %s - traceback: %s",
                         ex,
                         traceback.format_exc()
                     )
 
                     errors["base"] = "Error connecting to {ip}"
-                except Exception as ex:  # pylint: disable=broad-except
+                except Exception as ex:
                     _LOGGER.error(
                         f"{DOMAIN} Exception: %s - traceback: %s",
                         ex,
@@ -167,38 +194,33 @@ class SiemensS7ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="s7",
             data_schema=vol.Schema(
                 {
+                    vol.Required("name"): vol.All(str, vol.Length(2, 128)),
                     CONF_IP_ADDRESS: str,
-                    vol.Required("rack"): vol.All(int, vol.Clamp(0, 63)),
-                    vol.Required("slot"): vol.All(int, vol.Clamp(0, 63)),
+                    vol.Required("rack"): vol.All(int, vol.Range(0, 63)),
+                    vol.Required("slot"): vol.All(int, vol.Range(0, 63)),
                 }
             ),
             errors=errors ,
             description_placeholders={
+                "name": name,
                 "ip": ip,
                 "rack": rack,
                 "slot": slot
             }
         )
 
-    def validate_ip(self, ip) -> bool:
+    @staticmethod
+    def validate_ip(ip) -> bool:
         try:
             ipaddress.IPv4Address(ip)
             return True
         except ValueError:
             return False
-            
-    def validate_rack(self, rack) -> bool:
-        # TODO: check range
-        return type(rack) is int
 
-    def validate_slot(self, slot) -> bool:
-        # TODO: check range
-        return type(slot) is int
+    @staticmethod
+    def validate_rack_slot(slot) -> bool:
+        return type(slot) is int and RACKSLOT_MIN <= slot <= RACKSLOT_MAX
 
-    def validate_tsap_id(self, tsap_id) -> bool:
-        # TODO: use regular expression to check if it is a hex string
-        return len(tsap_id) == 4
-
-    def get_lib_location(self) -> str:
-        # TODO: find cpu architecture and os en determine library to be used
-        return f"{os.path.dirname(os.path.abspath(__file__))}/lib/linux_x86_x64/libsnap7.so"
+    @staticmethod
+    def validate_tsap_id(tsap_id) -> bool:
+        return len(tsap_id) == 4 and re.match(r'^[a-fA-F0-9]+$', tsap_id)
